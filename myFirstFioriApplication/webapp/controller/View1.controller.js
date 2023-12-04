b4e06070007ee6c19b6f9154c51b8319ab439b83
Sharma <kausik.sharma@accenture.com>
2023-12-04 22:41:14 +0530

    add searchfield filter implementation, add filter button on footer

--- myFirstFioriApplication/webapp/controller/View1.controller.js
+++ myFirstFioriApplication/webapp/controller/View1.controller.js
@@ -1,17 +1,41 @@
-sap.ui.define([
-    'fiori/practice/controller/BaseController',
-    'fiori/practice/model/models'
-], function(BaseController,Models) {
-    'use strict';
-    return BaseController.extend("fiori.practice.controller.View1",{
-        onInit: function(){
-           var oFruitModel = Models.createFruitModel("model/mockData/fruits.json");
-            this.getView().setModel(oFruitModel);
-        },
-        goToView1:function(){
-            debugger;
-            var oApp = this.getAppObject();
-            oApp.to("idView2");
-        }
+sap.ui.define(
+  ["fiori/practice/controller/BaseController", "fiori/practice/model/models","sap/ui/core/Fragment"],
+  function (BaseController, Models,Fragment) {
+    "use strict";
+    return BaseController.extend("fiori.practice.controller.View1", {
+      onInit: function () {
+        var oFruitModel = Models.createFruitModel("model/mockData/fruits.json");
+        this.getView().setModel(oFruitModel);
+      },
+      goToView1: function () {
+        debugger;
+        var oApp = this.getAppObject();
+        oApp.to("idView2");
+      },
+      _onSearch: function (oEvent) {
+        var value = oEvent.getParameter("newValue");
+        var oFilter1 = new sap.ui.model.Filter(
+          "name",
+          sap.ui.model.FilterOperator.Contains,
+          value
+        );
+        var oFilter2 = new sap.ui.model.Filter(
+          "benefit",
+          sap.ui.model.FilterOperator.Contains,
+          value
+        );
+        var oMainFilter = new sap.ui.model.Filter({
+          filters: [oFilter1, oFilter2],
+          and : false
+        });
+        var aFilter = [oMainFilter];
+        var oList = this.getView().byId("myList")
+        oList.getBinding("items").filter(aFilter);
+        debugger;
+      },
+      _onPressFilter: function (oEvent) {
+        
+      }
     });
-});
+  }
+);
