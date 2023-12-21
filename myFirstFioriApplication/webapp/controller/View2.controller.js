67c2dead5e75fb78d4f0117aad8db86a06f2f00a
Sharma <kausik.sharma@accenture.com>
2023-12-21 21:31:15 +0530

    addRow fragment added

--- myFirstFioriApplication/webapp/controller/View2.controller.js
+++ myFirstFioriApplication/webapp/controller/View2.controller.js
@@ -1,14 +1,53 @@
 sap.ui.define([
-    'fiori/practice/controller/BaseController'
-], function(BaseController) {
+    'fiori/practice/controller/BaseController',
+    'sap/m/MessageToast',
+    'sap/m/MessageBox'
+], function (BaseController, MessageToast, MessageBox) {
     'use strict';
-    return BaseController.extend("fiori.practice.controller.View2",{
-        onInit: function(){
-           
+    return BaseController.extend("fiori.practice.controller.View2", {
+        onInit: function () {
+
         },
-        onBack:function(){
+        onBack: function () {
             var oApp = this.getAppObject();//get the parent object
             oApp.to("idEmptyView");
+        },
+        approveClose:function(choice){
+            if(choice === 'OK'){
+                debugger;
+            }
+            else if(choice === 'CANCEL'){
+                debugger;
+            }
+        },
+        __onApprove: function (oEvent) {
+            MessageBox.confirm("Please confirm", {
+                title: "Confirm",                                    // default
+                onClose: this.approveClose.bind(this),                                       // default
+                styleClass: "",                                      // default
+                actions: [sap.m.MessageBox.Action.OK,
+                sap.m.MessageBox.Action.CANCEL],         // default
+                emphasizedAction: sap.m.MessageBox.Action.OK,        // default
+                initialFocus: null,                                  // default
+                textDirection: sap.ui.core.TextDirection.Inherit,
+                
+            })
+        },
+        onAdd:function(oEvent){
+            debugger;
+            var columnArr = Object.keys(this.getView().getModel().getProperty("/suppliers")[0]);
+            if (!this._oPopover) {
+                this._oPopover = new sap.ui.xmlfragment("fiori.practice.fragments.addRow", this);
+                this.getView().addDependent(this._oPopover);
+                this._oPopover.open(oEvent.getSource());
+                debugger;
+              }
+              else {
+                this._oPopover.open(oEvent.getSource());
+                //this._pPopover=undefined;
+              }
+            
         }
+
     });
 });
