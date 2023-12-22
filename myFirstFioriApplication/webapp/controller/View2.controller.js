e06b40f32319150543270721f645d2dfbc2ea28a
Sharma <kausik.sharma@accenture.com>
2023-12-22 20:37:56 +0530

    updated

--- myFirstFioriApplication/webapp/controller/View2.controller.js
+++ myFirstFioriApplication/webapp/controller/View2.controller.js
@@ -12,11 +12,11 @@ sap.ui.define([
             var oApp = this.getAppObject();//get the parent object
             oApp.to("idEmptyView");
         },
-        approveClose:function(choice){
-            if(choice === 'OK'){
+        approveClose: function (choice) {
+            if (choice === 'OK') {
                 debugger;
             }
-            else if(choice === 'CANCEL'){
+            else if (choice === 'CANCEL') {
                 debugger;
             }
         },
@@ -30,59 +30,59 @@ sap.ui.define([
                 emphasizedAction: sap.m.MessageBox.Action.OK,        // default
                 initialFocus: null,                                  // default
                 textDirection: sap.ui.core.TextDirection.Inherit,
-                
+
             })
         },
-        supplierAppendColumn:['name','city'],
-        onAdd:function(oEvent){
+        supplierAppendColumn: ['name', 'city'],
+        onAdd: function (oEvent) {
             debugger;
             var aSuppliersKeys = Object.keys(this.getView().getModel().getProperty("/suppliers")[0]);
             if (!this._oPopover) {
                 this._oPopover = new sap.ui.xmlfragment("fiori.practice.fragments.addRow", this);
                 this.getView().addDependent(this._oPopover);
-              }
-              
-              //Get the combobox data from addRow.fragment 
-              var oAddRowComboBox = sap.ui.getCore().byId("addRowComboBox");
-              oAddRowComboBox.removeAllItems()
-              oAddRowComboBox.clearSelection()
-              var columnArr = aSuppliersKeys.filter((element) => !this.supplierAppendColumn.includes(element));
+            }
 
-              for (let index = 0; index < columnArr.length; index++) {
-                  oAddRowComboBox.addItem(new sap.ui.core.Item({
-                      key:columnArr[index] ,
-                      text: columnArr[index]
-                  }))
-                  
-              }
-              this._oPopover.open(oEvent.getSource());
-            
+            //Get the combobox data from addRow.fragment 
+            var oAddRowComboBox = sap.ui.getCore().byId("addRowComboBox");
+            oAddRowComboBox.removeAllItems()
+            oAddRowComboBox.clearSelection()
+            var columnArr = aSuppliersKeys.filter((element) => !this.supplierAppendColumn.includes(element));
+
+            for (let index = 0; index < columnArr.length; index++) {
+                oAddRowComboBox.addItem(new sap.ui.core.Item({
+                    key: columnArr[index],
+                    text: columnArr[index]
+                }))
+
+            }
+            this._oPopover.open(oEvent.getSource());
+
         },
-        onAddRowOk:function(oEvent){
+        onAddRowOk: function (oEvent) {
             debugger;
             var getSelectedItem = sap.ui.getCore().byId("addRowComboBox").getSelectedItem()
-            if(getSelectedItem){
+            if (getSelectedItem) {
                 var colName = getSelectedItem.getText()
                 var oSupplierTable = this.getView().byId("__idSuppliersTable")
                 var columnListTable = this.getView().byId("__tableColList")
 
-                oSupplierTable.addColumn( new sap.m.Column({
+                oSupplierTable.addColumn(new sap.m.Column({
                     header: new sap.m.Label({
-                        text:colName //data[0].KURZNAME
+                        text: colName 
                     })
                 }));
-                columnListTable.addCell(new sap.m.Text({text: "{"+colName+"}" })); 
-                oSupplierTable.bindItems("/suppliers",columnListTable)
+                columnListTable.addCell(new sap.m.Text({ text: "{" + colName + "}" }));
+                oSupplierTable.bindItems("/suppliers", columnListTable)
                 this.supplierAppendColumn.push(colName);
                 this._oPopover.close();
 
             }
         },
-        
+
         onAddRowClose: function () {
-            if(this._oPopover){
+            if (this._oPopover) {
                 this._oPopover.close();
-   
+
             }
         }
 
